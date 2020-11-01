e2addc2fbc7a38ff38189f439720a2e6fd4122fb
Richard Macklin <richard.github@nrm.com>
2020-11-01 09:21:53 -0800

    Enable simplecov console formatter
    We upload the HTML coverage report to the GitHub Actions artifacts,
    whose storage expires after 90 days. By adding the console formatter,
    we'll also print a coverage summary directly in the CI log, which
    persists forever. The console reporter just shows line numbers in the
    output, though, whereas the HTML reports display the actual code with
    each line highlighted according to its coverage. Additionally, the
    console reporter doesn't handle branch coverage at this time. So it's
    worth using both formatters.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 if ENV['MEASURE_COVERAGE'] == 'true'
   require 'simplecov'
+  require 'simplecov-console'
+
   SimpleCov.start do
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::Console,
+    ])
     minimum_coverage 100
   end
 end
